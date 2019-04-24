f9f2eb6b26ab7dc0971d54fa7a62feb26749b04d
John Word <jword@sylvaniaschools.local>
2019-04-24 11:20:39 -0400

    event cards

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-$(document).ready(function() {
+let events = [
+    {'name':'Volleyball',
+     'spots':{'9':10,'10':10,'11':10,'12':10}},
+    {'name':'Basketball',
+    'spots':{'9':10,'10':10,'11':10,'12':10}}]
 
-});
+$('div#events-container').append(events.map((event) => 
+    $(`<div id='${event.name}' class='event-card card'>
+        <h1>${event.name}</h1>
+        <ul>
+            <li>9th: ${event.spots['9']}</li>
+        </ul>
+    </div>`)
+))
+
