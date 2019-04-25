b547fd3eb2334a15fcdb927f899e8085120316c9
John Word <jword@sylvaniaschools.local>
2019-04-25 08:25:57 -0400

    displays all event data

--- script.js
+++ script.js
@@ -3,20 +3,21 @@ let events = [
      'spots':{'9':10,'10':10,'11':10,'12':10}},
     {'name':'Basketball',
     'spots':{'9':10,'10':10,'11':10,'12':10}}]
-
-$('div#events-container').append(events.map((event) => 
-    $(`<div id='${event.name}' class='event-card card'>
-        <h1>${event.name}</h1>
+function createEventCards(events) {
+return events.map((event) => 
+    $(`<div id='${event[0]}' class='event-card card'>
+        <h3>${event[0]}</h3>
         <ul>
-            <li>9th: ${event.spots['9']}</li>
+            ${event.map(bit => `<li>${bit}</li>`).join('')}
         </ul>
     </div>`)
-))
+)
+}
 
 axios.post('https://script.google.com/macros/s/AKfycbwFPH7SSqFZ3Tn-nDR4DGlkzGMJK0KnRSXlO7wY2QTgfqapdoc/exec')
     .then((res) => {
         console.log(res)
-        $('body').append(res)
+        $('.container').append(createEventCards(res.data))
     }).catch((error) => {
         console.error(error)
     })
