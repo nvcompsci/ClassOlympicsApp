71819f818c49bcfb34e97a5691f5d7975fa6b875
John Word <jword@sylvaniaschools.local>
2019-04-29 15:44:04 -0400

    fully functional, not finished

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const url = 'https://script.google.com/macros/s/AKfycbwFPH7SSqFZ3Tn-nDR4DGlkzGMJK0KnRSXlO7wY2QTgfqapdoc/exec'
+const devUrl = 'https://script.google.com/a/sylvaniaschools.org/macros/s/AKfycbx-fJD1dQlWlvZz9eg0YH4ahICo96YWlQLVSgxYLrY/dev'
 
 const h = {
     'name':0,
@@ -7,10 +8,27 @@ const h = {
     'location':5,
     'desc':7
 }
+
+let data = {}
+
+$('.hideAtStart').hide()
+
+axios.post(url + "?route=getEvents")
+.then((res) => {
+    console.log(res)
+    data = res.data;
+    $('#events-container').append(createEventCards(res.data.events))
+    $('div.event-card button').click(handleButtonClick)
+    $('div.event-card').hover(mouseEnterCard, mouseLeaveCard)
+}).catch((error) => {
+    document.append(error);
+    console.error(error)
+})
+
 function createEventCards(events) {
 return events.map((event) => 
     $(`<div id='${event[h.name]}' class='event-card card'>
-        <h3>${event[h.name]}</h3>
+        <h6>${event[h.name]}</h6>
         <ul>
             <li>${event[h.desc]}</li>
             <li>${event[h.location]}</li>
@@ -22,19 +40,58 @@ return events.map((event) =>
 )
 }
 
-$("input[type='submit']").click(e => {
+$("button#login").click(e => {
     e.preventDefault()
     const sid = $('input[type="number"]').val()
-    axios.post(url)
+    axios.post(`${url}?route=login&student=${sid}`)
         .then((res) => {
+            data.spots = res.data.spots
+            data.student = res.data.student
+            data.n = res.data.n
             console.log(res)
-            $('.container').append(createEventCards(res.data))
-            $('div.event-card button').click(function() {$(this).toggleClass('btn-primary').toggleClass('btn-success')})
+            $('input[name=first]').val(res.data.student[data.n.s['First_Name']])
+            $('input[name=last]').val(res.data.student[data.n.s['Last_Name']])
+            $('input[name=grade]').val(res.data.student[data.n.s['Grade_Level']])
+            $('select').val(res.data.student[data.n.s['Event']])
+            $('.hideAtStart').show()
+            $('#events-container').empty().append(createEventCards(data.events))
+            $('div.event-card button').click(handleButtonClick)
         }).catch((error) => {
-            document.write(error);
             console.error(error)
-    })
+        })
 })
+
+$("button#signup").click(e => {
+    e.preventDefault()
+    const event = $('select').val()
+    const sid = $('input[type="number"]').val()
+    axios.post(`${url}?route=signup&student=${sid}&event=${event}`)
+        .then((res) => {
+            data.spots = res.data.spots
+            data.student = res.data.student
+            data.n = res.data.n
+            console.log(res)
+        }).catch((error) => {
+            console.error(error)
+        })
+})
+
+function handleButtonClick(e) {
+    let thisElem = $(e.target)
+    thisElem.toggleClass('btn-primary').toggleClass('btn-success')
+    let thisEvent = thisElem.siblings('h6').text();
+    $("select").val(thisEvent).show()
+}
+
+function mouseEnterCard() {
+    $(this).children('ul').toggle()
+    $(this).css("width","45%")
+}
+
+function mouseLeaveCard() {
+    $(this).children('ul').toggle()
+    $(this).css("width","22%")
+}
 
 function headerInds(headers) {
     var inds = {};
